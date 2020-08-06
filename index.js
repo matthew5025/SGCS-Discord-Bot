e4381970e9bdc649ce8814e2f9dfb01fde140759
dallas-ng <ngdallas1@gmail.com>
2020-08-06 17:09:07 +0800

    Added discord.js commando

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-require('dotenv').config()
+const { CommandoClient } = require('discord.js-commando')
+const path = require('path')
+const { token, prefix, discord_owner_id } = require('./config.json')
+
+const client = new CommandoClient({
+    commandPrefix: prefix,
+    owner: discord_owner_id,
+    invite: 'https://discord.gg/eXqcbDp',
+})
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'))
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
+
+    client.user.setActivity(`${prefix}help`, {
+        type: 'LISTENING',
+        url: 'https://github.com/dallas-ng',
+    })
 })
 
 client.on('message', (msg) => {
@@ -12,4 +30,4 @@ client.on('message', (msg) => {
     }
 })
 
-client.login(process.env.TOKEN)
+client.login(token)
